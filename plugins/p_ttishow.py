2e748199a8dc1145f069f24803d8a554c7df63cf
belikeberlinQ <116558839+belikeberlinQ@users.noreply.github.com>
2022-11-17 23:25:40 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -8,7 +8,7 @@ from utils import get_size, temp, get_settings
 from Script import script
 from pyrogram.errors import ChatAdminRequired
 
-"""-----------------------------------------https://t.me/TAMIL_MOVIES_UPDATE_ms --------------------------------------"""
+"""-----------------------------------------https://t.me/Srilinks4k --------------------------------------"""
 
 @Client.on_message(filters.new_chat_members & filters.group)
 async def save_group(bot, message):
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/+AaJO1Ej6T441YjQ1')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/Srilinks4k')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
